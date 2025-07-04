21c253ab7829aaec9f2b6a1e8effeb15af03b5d3
Kaetherine <kuhle.katherine@googlemail.com>
2025-07-04 12:45:11 +0200

    mark active page in nacbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,10 +4,31 @@ function loadNavbar() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
+            // Set active page after navbar is loaded
+            setActivePage();
         })
         .catch(error => {
             console.error('Error loading navbar:', error);
         });
+}
+
+// Function to set active page
+function setActivePage() {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    const navLinks = document.querySelectorAll('.navbar-container a:not(.navbar-brand)');
+    
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute('href').split('/').pop().split('#')[0];
+        
+        // Check if current page matches link
+        if (linkPage === currentPage || 
+            (currentPage === 'index.html' && linkPage === 'index.html') ||
+            (currentPage === '' && linkPage === 'index.html')) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
 }
 
 // Load navbar when page loads
